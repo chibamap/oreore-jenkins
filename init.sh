312f552b612c590780401d033ec929031a528699
tchibana <tsuk.316.banana@gmail.com>
2017-01-19 00:28:23 +0900

    re-runnable init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+
+set -e
+
 _pwd=$(cd -P `dirname $0` && pwd)
 
-ssh-keygen -t rsa -N "" -f $_pwd/id_key
+keygen(){
+  if [ -f $_pwd/slave/id_key ]; then
+    echo 'slave key already exists. skip it.'
+    return
+  fi
+  ssh-keygen -t rsa -N "" -f $_pwd/slave/id_key
+}
+
+
+keygen
